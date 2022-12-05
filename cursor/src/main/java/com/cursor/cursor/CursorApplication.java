2820fd844fda7d89f6531fdfb6f54f22fdd8ed04
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-12-05 22:00:43 +0200

    c5

--- cursor/src/main/java/com/cursor/cursor/CursorApplication.java
+++ cursor/src/main/java/com/cursor/cursor/CursorApplication.java
@@ -1,16 +1,22 @@
 package com.cursor.cursor;
 
+import com.cursor.cursor.service.CatFactService;
+import com.cursor.cursor.service.CatFactServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class CursorApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication.run(CursorApplication.class, args);
 
+		CatFactService catFactService = new CatFactServiceImpl();
 
-
+		System.out.println(catFactService.getCatFact().toString());
 
 	}
 
--- cursor/src/main/java/com/cursor/cursor/rest/restController.java
+++ cursor/src/main/java/com/cursor/cursor/rest/restController.java
@@ -1,17 +1,26 @@
 package com.cursor.cursor.rest;
 
+import com.cursor.cursor.responseModel.CatFactResponseModel;
+import com.cursor.cursor.service.CatFactService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(value = "/")
 public class restController {
 
-@GetMapping(value = "/getCatFact")
- public void getCatFact()
-{
+ private final CatFactService catFactService;
 
+ public restController(CatFactService catFactService) {
+  this.catFactService = catFactService;
+ }
+
+ @GetMapping(value = "/getCatFact")
+ public CatFactResponseModel getCatFact() throws IOException, InterruptedException {
+  return catFactService.getCatFact();
 }
 
 
